bbbdb72ee3a5dbed43efd542af3f80f406650d7e
steve-sanogo <sanogosteve@gmail.com>
2025-09-17 15:31:45 +0200

    Update ListeSimpleTest.java

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -49,7 +49,7 @@ class ListeSimpleTest {
         listeATester.ajout(1);
         listeATester.ajout(2);
         listeATester.ajout(3);
-        assertEquals("1->2->3", listeATester.toString());
+        assertEquals("ListeSimple(Noeud(3), Noeud(2), Noeud(1))", listeATester.toString());
         System.out.println(listeATester);
     }
 
